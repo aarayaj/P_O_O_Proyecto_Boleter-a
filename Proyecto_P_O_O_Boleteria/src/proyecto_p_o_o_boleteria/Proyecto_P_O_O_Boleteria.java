1009f2e59854c526c498a201a87125c7fc8d7e73
aarayaj <aarayaj@ucenfotec.ac.cr>
2020-11-28 07:57:23 -0600

    Proyecto_Incialización
    Inicialización_Proyecto

---
+++ Proyecto_P_O_O_Boleteria/src/proyecto_p_o_o_boleteria/Proyecto_P_O_O_Boleteria.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_p_o_o_boleteria;
+
+/**
+ *
+ * @author aaj
+ */
+public class Proyecto_P_O_O_Boleteria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

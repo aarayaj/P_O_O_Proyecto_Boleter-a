0edc81e563d26e910bb376c77de8e03116b2b17c
aarayaj <aarayaj@ucenfotec.ac.cr>
2020-12-18 23:53:05 -0600

    Actualizacion de Trabajo 3

--- Proyecto_P_O_O_Boleteria/src/zonasteatro/GraderiaPreferencial.java
+++ Proyecto_P_O_O_Boleteria/src/zonasteatro/GraderiaPreferencial.java
@@ -5,12 +5,15 @@ import java.util.Stack;
 public class GraderiaPreferencial extends Graderia{
     
     private Stack<Integer> espacio;
-    private final int CANTIDAD_ESPACIO = 25;
+    private final int CANTIDAD_ESPACIO = 5;
     private final double COSTO_BOLETO = 5500.00;
     
     public GraderiaPreferencial() {
         super();
         this.espacio = new Stack();
+        for (int i = 0; i < CANTIDAD_ESPACIO-1; i++){
+            this.espacio.push(super.getESPACIO_LIBRE());
+        }
     }
     
     @Override
@@ -41,6 +44,31 @@ public class GraderiaPreferencial extends Graderia{
     @Override
     public void setEspacioReservadoLibre(int eje_x) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEspaciosBasico(int eje_x, int rOpcion) {
+        Stack<Integer> oldEspacio = espacio;
+        Stack<Integer> newEspacio = new Stack<>();
+        espacio = new Stack<>();
+        
+        // 1 para agregar Reserva
+        if (rOpcion == 0){
+            while (oldEspacio.peek() == 0){
+                oldEspacio.pop();
+            }
+            while (oldEspacio.peek() != 0 && oldEspacio.peek() != null){
+                newEspacio.push(oldEspacio.pop());
+            }
+            while (newEspacio.peek() != null) {
+                espacio.push(newEspacio.pop());
+            }
+            
+        }
+        // 2 para agregar Pagados
+        if (rOpcion == 1){
+            
+        }
     }
     
 }

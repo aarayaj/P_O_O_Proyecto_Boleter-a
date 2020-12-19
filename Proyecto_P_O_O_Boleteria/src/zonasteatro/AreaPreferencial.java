ac063324d2cad8897e834006e52132821de6dda3
aarayaj <aarayaj@ucenfotec.ac.cr>
2020-12-19 10:08:05 -0600

    Actualizacion de Trabajo Final 1

--- Proyecto_P_O_O_Boleteria/src/zonasteatro/AreaPreferencial.java
+++ Proyecto_P_O_O_Boleteria/src/zonasteatro/AreaPreferencial.java
@@ -13,7 +13,7 @@ public class AreaPreferencial extends Graderia{
     public AreaPreferencial() {
         super();
         this.espacio = new ArrayList<>(CANTIDAD_ESPACIO);
-        for (int i = 0; i < CANTIDAD_ESPACIO-1; i++){
+        for (int i = 0; i < CANTIDAD_ESPACIO; i++){
             this.espacio.add(super.getESPACIO_LIBRE());
         }        
     }
@@ -23,29 +23,60 @@ public class AreaPreferencial extends Graderia{
         return this.COSTO_BOLETO;
     }
 
-    @Override
+    @Override 
     public void setEspacioReservado(int eje_x) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        espacio.set(eje_x, getESPACIO_RESERVADO());
     }
 
     @Override
     public void setEspacioOcupado(int eje_x) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        espacio.set(eje_x, getESPACIO_OCUPADO());
     }
 
-    @Override
-    public boolean getEspacioLibre(int eje_x) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
+    @ Override
     public void setEspacioReservadoOcupado(int eje_x) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void setEspacioReservadoLibre(int eje_x) {
+        espacio.set(eje_x, getESPACIO_LIBRE());
+    }
+
+    @Override
+    public void setEspaciosBasico(int eje_x, int rOpcion) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    @Override
+    public void setEspaciosCompuesto(int eje_x, int rOpcion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setInformacionEspacio() {
+        espacio.forEach(value -> {
+            if (value == 0) {
+                setEspaciosLibres(getEspaciosLibres() +1);
+            }
+            if (value == 1) {
+                setEspaciosReservados(getEspaciosReservados()+1);
+            }
+            if (value == 2) {
+                setEspaciosOcupados(getEspaciosOcupados() +1);
+            }
+        });
+        setTotalGanancia(getEspaciosOcupados() * COSTO_BOLETO);
+    }
+
+    @Override
+    public void reducirListaDeEspera() {
+        if (getListaDeEspera().peek() != null){
+            for (int i = 0; i < CANTIDAD_ESPACIO; i++){
+                if (espacio.get(i) == 0){
+                    espacio.set(i, 2);
+                }
+            }
+        }
+    }
 }
